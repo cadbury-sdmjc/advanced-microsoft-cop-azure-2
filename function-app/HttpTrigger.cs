9bb4303412be2714ffb46c8c2cc7e49cee4da563
Sam MC <sam+github@sdmjc.net>
2022-11-29 12:05:40 +0000

    add demo functionality to function app

--- function-app/HttpTrigger.cs
+++ function-app/HttpTrigger.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,11 +6,10 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace function_app;
 
-public record QueueMessage(int Tenant, string Content)
+public record QueueMessage(int Tenant, Guid Id, string Content)
 {
     public const string QueueName = "demo-queue";
 };
@@ -20,13 +18,15 @@ public static class HttpTrigger
 {
     [FunctionName("HttpTrigger")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
-        [Queue(QueueMessage.QueueName)] IAsyncCollector<QueueMessage> collector,
+        [HttpTrigger(AuthorizationLevel.Function, "post")]
+        HttpRequest req,
+        [Queue(QueueMessage.QueueName, Connection = "AdvancedMicrosoftCop")]
+        IAsyncCollector<QueueMessage> collector,
         ILogger log)
     {
         foreach (var i in Enumerable.Range(0, 100))
         {
-            await collector.AddAsync(new(i, $"{Guid.NewGuid()}"));
+            await collector.AddAsync(new(i, Guid.NewGuid(), $"some message content goes in here!\r\nCreated at {DateTime.UtcNow:O}"));
         }
 
         return new NoContentResult();
--- function-app/QueueTrigger.cs
+++ function-app/QueueTrigger.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Storage.Blobs;
 using Microsoft.Azure.WebJobs;
 
 namespace function_app;
@@ -7,9 +9,12 @@ namespace function_app;
 public static class QueueTrigger
 {
     [FunctionName("QueueTrigger")]
-    public static async Task Run([QueueTrigger(QueueMessage.QueueName)] QueueMessage message)
+    public static async Task Run([QueueTrigger(QueueMessage.QueueName, Connection = "AdvancedMicrosoftCop")] QueueMessage message)
     {
         await Task.Delay(100 * Random.Shared.Next(5, 15));
-        // write message contents to storage account
+        var containerName = "from-function-app";
+        var miClient = new BlobContainerClient(new($"https://advmsftcop.blob.core.windows.net/{containerName}"), new DefaultAzureCredential());
+        var blob = miClient.GetBlobClient($"{message.Tenant}/{message.Id}");
+        await blob.UploadAsync(BinaryData.FromString(message.Content + $"\r\nWritten to blob at: {DateTime.UtcNow:O}"));
     }
 }
